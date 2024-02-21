06856b88a24aa40889ec46cb2a6a8b35b0645732
specialJamiKim <xofyd0038@naver.com>
2024-02-22 08:39:47 +0900

    Initial commit

---
+++ src/main/java/com/projectA1/ProjectA1Application.java
@@ -0,0 +1,13 @@
+package com.projectA1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectA1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectA1Application.class, args);
+	}
+
+}
