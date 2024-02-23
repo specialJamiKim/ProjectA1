8d02cfc0b387fb248472360e0fcd6b12f5af59e7
YoonSungH <skylife50@naver.com>
2024-02-23 16:44:06 +0900

    12

--- src/main/java/com/projectA1/repository/ReviewRepository.java
+++ src/main/java/com/projectA1/repository/ReviewRepository.java
@@ -1,11 +1,17 @@
 package com.projectA1.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 import com.projectA1.model.Review;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Long>{
 	
+	@Query( "select sc from Review sc where sc.review.id=?")
+	public List<Review> findByid(Long id);
 }
