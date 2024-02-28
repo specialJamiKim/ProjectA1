e4343c5eb71a264e04ba1766fcd3bfc665611f20
specialJamiKim <xofyd0038@naver.com>
2024-02-28 12:20:19 +0900

    .

--- src/main/java/com/projectA1/controller/HomeController.java
+++ src/main/java/com/projectA1/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -65,5 +66,14 @@ public class HomeController {
     }
    
 
+    // 임시 세션 초기화
+    @GetMapping("/sessionReset")
+    public String sessionReset(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        // 세션 초기화
+        session.invalidate();
+        // 초기화된 후에 어디로 이동할지 리다이렉트할 URL을 반환합니다.
+        return "redirect:/"; // 세션을 초기화한 후 메인 페이지로 리다이렉트할 수 있습니다. 필요에 따라 다른 경로를 지정할 수 있습니다.
+    }
     
 }
