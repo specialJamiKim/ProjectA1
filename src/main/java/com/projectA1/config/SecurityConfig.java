08f0848059f649ae349bf17a8c78cc7078c78829
specialJamiKim <xofyd0038@naver.com>
2024-02-29 14:32:45 +0900

    1

--- src/main/java/com/projectA1/config/SecurityConfig.java
+++ src/main/java/com/projectA1/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.projectA1.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,27 +23,27 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain fileChain(HttpSecurity http) throws Exception {
-	    http.csrf(csrf -> csrf.disable())
-	        .authorizeHttpRequests(authorize -> authorize
-	            .requestMatchers("/", "/main", "/join/**", "/login/**" , "/centerManage/**", "/diary/**", "/login/loginPage", "/user/join", "/owner/join", "/fragments/*").permitAll()
-	            .anyRequest().authenticated())
-	        .formLogin(login -> login
-	            .loginPage("/login/loginPage")
-	            .loginProcessingUrl("/loginPro")
-	            .defaultSuccessUrl("/", true)
-	            .permitAll())
-	        .logout(logout -> logout
-	            .logoutUrl("/logout")
-	            .logoutSuccessUrl("/")
-	            .invalidateHttpSession(true)
-	            .deleteCookies("JSESSIONID")
-	            .permitAll());
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(request -> request
+						.requestMatchers("/", "/main", "/join/**", "/login/**", "/centerManage/**", "/diary/**",
+								"/login/loginPage", "/user/join", "/owner/join", "/fragments/*")
+						.permitAll()
+						.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+						.requestMatchers("/img/**").permitAll().requestMatchers("/user/**").hasRole("USER")
+						.requestMatchers("/owner/**").hasRole("OWNER")
+						.anyRequest().authenticated())
+				.formLogin(login -> login.loginPage("/login/loginPage") // 로그인 페이지 URL 설정
+						.loginProcessingUrl("/loginPro") // 로그인 처리 URL 설정
+						.defaultSuccessUrl("/", true) // 로그인 성공 후 리다이렉트할 경로 설정
+						.permitAll())
+				.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/") // 로그아웃 후 리다이렉트할 경로 설정
+						.invalidateHttpSession(true) // 세션 무효화
+						.deleteCookies("JSESSIONID") // JSESSIONID 쿠키 삭제
+						.permitAll());
 
-	    return http.build();
+		return http.build();
 	}
 
-
-	
 	@Bean
 	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
 			throws Exception {
