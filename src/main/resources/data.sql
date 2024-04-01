5e87297ed2f784c32fc02ccc9dc33bdf1554056f
sanepark <xofyd0038@naver.com>
2024-04-01 16:38:54 +0900

    213
    213

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,23 @@
 INSERT INTO f_user (address, email, gender, join_date, name, password, phone_number, role, birth_date)
-VALUES ('부산광역시 금정구', '1@gmail.com', 'male', CURRENT_TIMESTAMP, '김덕배', '1', '010-1111-1111', 'ROLE_USER', '1995-04-28');
+VALUES ('부산광역시 부산진구 중앙대로 694 쥬디스태화', '1@gmail.com', 'male', CURRENT_TIMESTAMP, '김덕배', '1', '010-1111-1111', 'ROLE_USER', '1995-04-28');
 
 INSERT INTO f_user (address, email, gender, join_date, name, password, phone_number, role, birth_date)
-VALUES ('부산광역시 남구', '2@gmail.com', 'male', CURRENT_TIMESTAMP, '황덕구', '1', '010-4242-1919', 'ROLE_USER', '2002-07-11');
+VALUES ('부산광역시 부산진구 서전로10번길 61 쥬디스태화신관', '2@gmail.com', 'male', CURRENT_TIMESTAMP, '황덕구', '1', '010-4242-1919', 'ROLE_USER', '2002-07-11');
 
 INSERT INTO f_user (address, email, gender, join_date, name, password, phone_number, role, birth_date)
-VALUES ('서울특별시 강남구', '3@gmail.com', 'female', CURRENT_TIMESTAMP, '이숙녀', '1', '010-2222-2222', 'ROLE_USER', '2002-05-11');
+VALUES ('부산 부산진구 가야대로 772', '3@gmail.com', 'female', CURRENT_TIMESTAMP, '이숙녀', '1', '010-2222-2222', 'ROLE_USER', '2002-05-11');
 
 INSERT INTO f_center (address, closing_time, daily_pass_price, name, open_time, phone_number, image_path)
-VALUES ('부산광역시 부산진구' , '18:30' , 10000, '굿케어헬스', '09:00' , '010-1234-5678' , 'health1.png');
+VALUES ('부산 부산진구 중앙대로 708 부산파이낸스센터' , '18:30' , 10000, '굿케어헬스', '09:00' , '010-1234-5678' , 'health1.png');
 
 INSERT INTO f_center (address, closing_time, daily_pass_price, name, open_time, phone_number, image_path)
-VALUES ('부산광역시 부산진구' , '20:30' , 12000, '나이스헬스', '08:30' , '010-1234-5678'  , 'health2.png');
+VALUES ('부산광역시 부산진구 중앙대로 709' , '20:30' , 12000, '나이스헬스', '08:30' , '010-1234-5678'  , 'health2.png');
 
 INSERT INTO f_center (address, closing_time, daily_pass_price, name, open_time, phone_number, image_path)
-VALUES ('대구광역시 달서구' , '21:00' , 8000, '람보헬스', '09:00' , '010-1234-5678' , 'health3.png');
+VALUES ('부산광역시 부산진구 서전로10번길 61 쥬디스태화신관' , '21:00' , 8000, '람보헬스', '09:00' , '010-1234-5678' , 'health3.png');
 
 INSERT INTO f_center (address, closing_time, daily_pass_price, name, open_time, phone_number, image_path)
-VALUES ('울산광역시 중구' , '23:00' , 10000, '진짜로피트니스', '07:00' , '010-1234-5678' , 'health4.png');
+VALUES ('부산 부산진구 가야대로 772' , '23:00' , 10000, '진짜로피트니스', '07:00' , '010-1234-5678' , 'health4.png');
 
 INSERT INTO f_center (address, closing_time, daily_pass_price, name, open_time, phone_number, image_path)
 VALUES ('부산광역시 동래구' , '23:30' , 10000, '몸짱피트니스', '06:00' , '010-1234-5678 ', 'health5.png');
