16d76071700acb6ef406194ebb7048dd559d0bea
specialJamiKim <xofyd0038@naver.com>
2024-05-07 21:08:52 +0900

    11

--- src/main/java/com/projectA1/config/SecurityConfig.java
+++ src/main/java/com/projectA1/config/SecurityConfig.java
@@ -39,39 +39,39 @@ public class SecurityConfig {
 	////////////////////////////////////////////
 	// 삭제가능 ///
 
-	private static final ObjectMapper objectMapper = new ObjectMapper();
+//	private static final ObjectMapper objectMapper = new ObjectMapper();
+//
+//	public static String convertObjectToJson(Object object) throws JsonProcessingException {
+//		return objectMapper.writeValueAsString(object);
+//	}
 
-	public static String convertObjectToJson(Object object) throws JsonProcessingException {
-		return objectMapper.writeValueAsString(object);
-	}
-
-	@Bean
-	public AuthenticationSuccessHandler successHandler() {
-		return (request, response, authentication) -> {
-			PrincipalUser principalUser = (PrincipalUser) authentication.getPrincipal();
-			HttpSession session = request.getSession();
-			session.setAttribute("user", principalUser); // PrincipalUser 객체를 세션에 저장
-			session.setMaxInactiveInterval(1800); // 세션 만료 시간을 30분으로 설정 (초 단위)
-			System.out.println("세션에 사용자이름 >>" + principalUser.getUsername() + " 님이 저장됨");
-			response.setStatus(HttpServletResponse.SC_OK);
-
-			// Content-Type을 설정하여 UTF-8 문자 인코딩을 사용
-			response.setContentType("application/json; charset=UTF-8");
-			response.getWriter().write(convertObjectToJson(principalUser.getUser()));
-		};
-	}
-
-	@Bean
-	public SimpleUrlAuthenticationFailureHandler failureHandler() {
-		return new SimpleUrlAuthenticationFailureHandler() {
-			@Override
-			public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-					AuthenticationException exception) throws IOException, SecurityException {
-				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				response.getWriter().write("Login Failed");
-			}
-		};
-	}
+//	@Bean
+//	public AuthenticationSuccessHandler successHandler() {
+//		return (request, response, authentication) -> {
+//			PrincipalUser principalUser = (PrincipalUser) authentication.getPrincipal();
+//			HttpSession session = request.getSession();
+//			session.setAttribute("user", principalUser); // PrincipalUser 객체를 세션에 저장
+//			session.setMaxInactiveInterval(1800); // 세션 만료 시간을 30분으로 설정 (초 단위)
+//			System.out.println("세션에 사용자이름 >>" + principalUser.getUsername() + " 님이 저장됨");
+//			response.setStatus(HttpServletResponse.SC_OK);
+//
+//			// Content-Type을 설정하여 UTF-8 문자 인코딩을 사용
+//			response.setContentType("application/json; charset=UTF-8");
+//			response.getWriter().write(convertObjectToJson(principalUser.getUser()));
+//		};
+//	}
+//
+//	@Bean
+//	public SimpleUrlAuthenticationFailureHandler failureHandler() {
+//		return new SimpleUrlAuthenticationFailureHandler() {
+//			@Override
+//			public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+//					AuthenticationException exception) throws IOException, SecurityException {
+//				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//				response.getWriter().write("Login Failed");
+//			}
+//		};
+//	}
 
 	// 삭제예정///
 	///////////////////////////////////////////
@@ -80,27 +80,27 @@ public class SecurityConfig {
 	public SecurityFilterChain fileChain(HttpSecurity http) throws Exception {
 
 		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(request -> request
-				.requestMatchers("/m_visit/**", "/m_review/**", "/m_reservation/**", "/m_centerManage/**", "/m_user/**",
+				.requestMatchers(
 						"/", "/img/**", "/src/main/**", "/main", "/updatePassword", "/join/**", "/login/**",
 						"/centerManage/**", "/diary/**", "/test/**", "/login/**", "/user/join", "/owner/join",
 						"/fragments/*")
 				.permitAll().requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-				.requestMatchers("/img/**").permitAll().requestMatchers("/user/**").hasRole("USER")
+				.requestMatchers("/img/**").permitAll()
+				.requestMatchers("/user/**").hasRole("USER")
 				.requestMatchers("/owner/**").hasRole("OWNER").anyRequest().authenticated())
 
 		// 되는 부분, 원래 웹서버랑 연동하는 부분입니다. 나중에 살려서 쓰시면 잘 동작함
-//				.formLogin(login -> login.loginPage("/login/loginPage") // 로그인 페이지 URL 설정
-//						.loginProcessingUrl("/loginPro") // 로그인 처리 URL 설정
-//						.usernameParameter("email").passwordParameter("password")
-//						.defaultSuccessUrl("/", true) // 로그인 성공 후 리다이렉트할 경로 설정
-//						.permitAll())
+				.formLogin(login -> login.loginPage("/login/loginPage") // 로그인 페이지 URL 설정
+						.loginProcessingUrl("/loginPro") // 로그인 처리 URL 설정
+						.defaultSuccessUrl("/", true) // 로그인 성공 후 리다이렉트할 경로 설정
+						.permitAll())
 
 				// 안드로이드 통신 시큐리티 적용때문에 사용하는 부분
-				.formLogin(login -> login.loginPage("/login/loginPage") // 로그인 페이지 URL 설정 (필요한 경우) => 안드로이드 앱 사용시 사용 안함
-						.loginProcessingUrl("/loginPro") // 로그인 처리 URL 설정
-						.usernameParameter("email").passwordParameter("password") // 요청에서 사용되는 파라미터 설정 (필요한 경우)
-						.successHandler(successHandler()).failureHandler(failureHandler()).permitAll()) // 모든 사용자에게 접근
-																										// 허용
+//				.formLogin(login -> login.loginPage("/login/loginPage") // 로그인 페이지 URL 설정 (필요한 경우) => 안드로이드 앱 사용시 사용 안함
+//						.loginProcessingUrl("/loginPro") // 로그인 처리 URL 설정
+//						.usernameParameter("email").passwordParameter("password") // 요청에서 사용되는 파라미터 설정 (필요한 경우)
+//						.successHandler(successHandler()).failureHandler(failureHandler()).permitAll()) // 모든 사용자에게 접근
+//																										// 허용
 
 				.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/") // 로그아웃 후 리다이렉트할 경로 설정
 						.invalidateHttpSession(true) // 세션 무효화
